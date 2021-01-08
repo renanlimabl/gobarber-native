f16499a3bf5b6c31691a5548a1a0628b3036e9bb
renanlimabl <renanlimabl@gmail.com>
2021-01-08 12:31:08 -0300

    Adiciona fonts externas

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
-const SignIn: React.FC = () => <Container />;
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu Logon</Title>
+    </Container>
+  );
+};
 
 export default SignIn;
