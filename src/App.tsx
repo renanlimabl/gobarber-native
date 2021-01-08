84b40bc041a40a6fcb7328543ecfd785333120b6
renanlimabl <renanlimabl@gmail.com>
2021-01-08 10:50:46 -0300

    Adiciona dependencia de rotas e adiciona signin e signup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
+  <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#321e28" />
-    <View style={{ flex: 1, backgroundColor: '#321e28' }} />
-  </>
+    <View style={{ flex: 1, backgroundColor: '#321e28' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
 );
 
 export default App;
